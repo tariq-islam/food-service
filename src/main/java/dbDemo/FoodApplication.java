fffd2ecf8307c3b791a5976336947cdb24a73c52
Peter Larsen <plarsen@redhat.com>
2016-01-26 19:13:06 -0500

    Added Servlet Initializer for Tomcat

--- src/main/java/dbDemo/FoodApplication.java
+++ src/main/java/dbDemo/FoodApplication.java
@@ -26,6 +26,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @Configuration
 @ComponentScan
@@ -36,7 +37,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class FoodApplication{
+public class FoodApplication extends SpringBootServletInitializer {
 
 	@Autowired
     FoodRepository repository;
@@ -44,15 +45,4 @@ public class FoodApplication{
 	 public static void main(String[] args) {
 	        SpringApplication.run(FoodApplication.class);
 	    }
-
-	
-
-
-
-
-	
-	
-    
-    
-    
 }
